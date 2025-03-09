87d5ea32e00ba209bea9ffc33af9ed1995302456
123 <123>
2025-03-09 09:40:55 +0200

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import { Button } from '@/shared/ui/button';
-export default function Home() {
+import { prisma } from "@/shared/lib/db";
+import { Button } from "@/shared/ui/button";
+import { Card } from "@/shared/ui/card";
+export default async function Home() {
+  const games = await prisma.game.findMany();
+  console.log(games);
+
   return (
     <div>
+      {games.map((game) => (
+        <Card key={game.id}>{game.name}</Card>
+      ))}
       <Button>Hollo</Button>
     </div>
   );
